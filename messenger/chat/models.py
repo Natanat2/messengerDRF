a278ce7e35cb22f8f580f433ad1ef5a9f7d87ac0
Natanat2 <natanat2@mail.ru>
2024-04-04 00:59:46 +0600

    add avatar 1

--- messenger/chat/models.py
+++ messenger/chat/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    avatar = models.ImageField(upload_to = 'avatars/', null = True, blank = True)
+    avatar = models.ImageField(upload_to = 'images/profile/', null = True, blank = True)
 
     def __str__(self):
         return self.user.username

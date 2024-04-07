241de1affea072126dc999a9e9f1705b5d68fafe
Natanat2 <natanat2@mail.ru>
2024-04-07 17:51:01 +0600

    clean code 2

--- messenger/accounts/views.py
+++ messenger/accounts/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from .forms import SignUpForm, UserProfileForm
+from .forms import SignUpForm
 
 
 class SignUp(CreateView):
@@ -11,13 +9,3 @@ class SignUp(CreateView):
     success_url = '/accounts/login'
     template_name = 'registration/signup.html'
 
-
-def profile_view(request):
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES, instance = request.user.userprofile)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/profile/')
-    else:
-        form = UserProfileForm(instance = request.user.userprofile)
-    return render(request, 'profile.html', {'form': form})
